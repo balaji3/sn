9a3bf5426e75f315f46c04c75c82e4270be38dc3
balaji3 <47345402+balaji3@users.noreply.github.com>
2019-02-05 12:28:44 +0530

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,5 +1,5 @@
 ch = input()
 if((ch>='a' and ch<= 'z') or (ch>='A' and ch<='Z')):
-print(ch, "is a Alphabet")
+print(ch, "is Alphabet")
 else:
-print(ch, "is not a Alphabet")
+print(ch, "is not Alphabet")
