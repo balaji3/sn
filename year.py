a6e1bd5e88e0dfae1fd9dc0c521719bc6d5205c5
balaji3 <47345402+balaji3@users.noreply.github.com>
2019-02-05 12:56:15 +0530

    Update year.py

--- year.py
+++ year.py
@@ -1,7 +1,4 @@
-# Python program to check if the input year is a leap year or not
 year = 2000
-# To get year (integer input) from the user
-# year = int(input("Enter a year: "))
 if (year % 4) == 0:
 if (year % 100) == 0:
 if (year % 400) == 0:
