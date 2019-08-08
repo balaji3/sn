399d360cb30b78ec75d77e031779f11a992615fe
balaji3 <47345402+balaji3@users.noreply.github.com>
2019-08-08 10:14:20 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,8 +1,8 @@
-ch = input("Enter a character: ")
+ch = input()
 if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
 or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
     print("Vowel")
 elif(ch=='!' or ch=='@' or ch=='#' or ch=='$' or ch=='%' or ch=='^' or ch=='&' or ch=='*'):
     print("invalid")
 else:
-    print("consonant")
+    print("Consonant")
