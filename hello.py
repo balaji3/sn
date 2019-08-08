4d9a3a3c59e2c44f2eb642518b6541ade589b682
balaji3 <47345402+balaji3@users.noreply.github.com>
2019-08-08 15:01:27 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,4 @@
-#include <iostream>
-using namespace std;
-int main() {
-int i, n;
-cin >> n;
-for (i=0; i<n; i++) {
-cout << "hello world\n";
-}
-return 0;
-}
+n=int(input())
+a="Hellow"
+for x in range(n):
+    print(a)
